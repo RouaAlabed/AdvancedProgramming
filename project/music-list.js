c4425a9447da14ab1f744ed83e8722885356cb66
RouaAlabed <91522794+RouaAlabed@users.noreply.github.com>
2022-12-23 16:43:55 +0300

    Add files via upload

---
+++ project/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+      name: "Ney - Ruhumun sesi",
+      artist: "Hasan sari",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Peaceful sufi music",
+      artist: "Hakan Mengüç",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Enta omri - ney",
+      artist: "Mohamad Fityan",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Dezi muzigi",
+      artist: "Bilinmeyen",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Farsca ney oyunu",
+      artist: "ahmad al mosoui",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Bab al-hara",
+      artist: "Eyup",
+      img: "music-6",
+      src: "music-6"
+    },
+  ];
